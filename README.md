08bd428c1befef4b5b936acaaf37395e805bcdda
Tipene Moss <teee.moose@gmail.com>
2016-04-03 10:02:55 -0400

    updated init_db to work with app factory. Updated readme to reflect new method of starting app

--- README.md
+++ README.md
@@ -9,8 +9,5 @@
 
 1. `docker-compose build`
 2. `docker-compose up -d`
-3. `docker exec -i -t brightcore_project bash`
-4. `python init_db.py` -- Feel free to log out of the docker image after
-   this
-5. Access the site at localhost:32770
-
+3. `docker-compose run web python init_db.py`
+4. Access the site at localhost:32770
--- init_db.py
+++ init_db.py
@@ -1,17 +1,22 @@
-from flask_app.database import init_db, db_session
+from flask_app import create_app, db
 from flask_app.models import Client, Product
 
-init_db()
+def populate_tables(db_session):
+    if len(Client.query.all()) == 0:
+        cs = [Client('Client%s' % x) for x in ['A', 'B', 'C']]
 
-if len(Client.query.all()) == 0:
-	cs = [Client('Client%s' % x) for x in ['A', 'B', 'C']]
+        for c in cs:
+            db_session.session.add(c)
 
-	for c in cs:
-		db_session.add(c)
-	
-	p = Product('Policies'); db_session.add(p)
-	p = Product('Billing'); db_session.add(p)
-	p = Product('Claims'); db_session.add(p)
-	p = Product('Reports'); db_session.add(p)
+        p = Product('Policies'); db_session.session.add(p)
+        p = Product('Billing'); db_session.session.add(p)
+        p = Product('Claims'); db_session.session.add(p)
+        p = Product('Reports'); db_session.session.add(p)
 
-	db_session.commit()
+        db_session.session.commit()
+
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+        populate_tables(db)
